49a9f6da32bc6e938c0ae2752669d636ed22505d
Josef Patoprsty <seppi@josefnpat.com>
2013-04-22 01:45:33 -0400

    Closes #1 This will map everything properly for windows (with exception of concurrent LT and RT). This relates to Issue #2

--- dong.lua
+++ dong.lua
@@ -23,7 +23,62 @@ dong.os["OS X"] = {
   end
 }
 
-dong.os["Windows"] = dong.os["Linux"]
+dong.os["Windows"] = {
+  _getAxes = function(joystick)
+    local temp = {}
+    local trigger = nil
+    temp.ls_x,temp.ls_y,trigger,temp.rs_y,temp.rs_x = love.joystick.getAxes(joystick)
+    if trigger < 0 then
+      temp.rt = -trigger*2-1
+      temp.lt = -1
+    elseif trigger > 0 then
+      temp.lt = trigger*2-1
+      temp.rt = -1
+    else -- trigger == 0
+      temp.rt,temp.lt = -1,-1
+    end
+    return temp
+  end,
+  buttons = function(joystick)
+    return {"A","B","X","Y","LB","RB","SELECT","START","LS","RS","RD","UD","LD","DD"}
+  end,
+  extra_buttons = function(joystick,button)
+    local direction = love.joystick.getHat( joystick, 1 )
+    if direction == "r" then
+      if button == 11 then
+        return true
+      end
+    elseif direction == "u" then
+      if button == 12 then
+        return true
+      end
+    elseif direction == "l" then
+      if button == 13 then
+        return true
+      end
+    elseif direction == "d" then
+      if button == 14 then
+        return true
+      end
+    elseif direction == "ru" then
+      if button == 11 or button == 12 then
+        return true
+      end
+    elseif direction == "lu" then
+      if button == 13 or button == 12 then
+        return true
+      end
+    elseif direction == "ld" then
+      if button == 13 or button == 14 then
+        return true
+      end
+    elseif direction == "rd" then
+      if button == 11 or button == 14 then
+        return true
+      end
+    end
+  end
+}
 dong.os["Unknown"] = dong.os["Linux"]
 
 function dong._getAxes(joystick)
@@ -74,12 +129,21 @@ function dong.right_trigger(joystick)
   return temp.rt
 end
 
+function dong._isDown(joystick,i)
+  if dong.os[love._os].extra_buttons then
+    if dong.os[love._os].extra_buttons(joystick,i) then
+      return true
+    end
+  end
+  return love.joystick.isDown(joystick,i)
+end
+
 -- BUTTONS
 dong.buttons = dong.os[love._os].buttons()
 function dong.isDown(joystick,button_name)
   for i,button in ipairs(dong.buttons) do
     if button == button_name then
-      return love.joystick.isDown(joystick,i)
+      return dong._isDown(joystick,i)
     end
   end
   print("Invalid button.")
